ff88c81c0dd8cfec249885eec3fb20df66589f69
Kun Huang <kkun.huang@gmail.com>
2019-09-29 15:45:12 -0400

    Initial Commit

---
+++ TryReddit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TryReddit
+//
+//  Created by Kun Huang on 9/28/19.
+//  Copyright © 2019 Kun Huang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
